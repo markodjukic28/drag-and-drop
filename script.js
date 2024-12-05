87f9ff6bdf95a90dc931fac25a93e2ede1faf7d0
markodjukic28 <markogvs@gmail.com>
2024-12-05 21:34:13 +0100

    drag-&-drop

---
+++ script.js
@@ -0,0 +1,24 @@
+let lists = document.querySelectorAll('.list');
+let rightBox = document.getElementById('right');
+let leftBox = document.getElementById('left');
+
+lists.forEach(list => {
+  list.addEventListener('dragstart', function (e) {
+    let selected = e.target;
+
+    rightBox.addEventListener('dragover', function (e) {
+      e.preventDefault();
+    });
+    rightBox.addEventListener('drop', function (e) {
+      rightBox.appendChild(selected);
+      selected = null;
+    });
+    leftBox.addEventListener('dragover', function (e) {
+      e.preventDefault();
+    });
+    leftBox.addEventListener('drop', function (e) {
+      leftBox.appendChild(selected);
+      selected = null;
+    });
+  });
+});
